37ed906b7dae2e40e1af1bf0dec511ab842db8d1
TR <git@fin0c.net>
2015-10-17 23:14:26 -0500

    Modularize density plot, small ui fixes

--- density.js
+++ density.js
@@ -1,6 +1,266 @@
 
+    function kernelDensityEstimator(kernel, x) {
+      return function(sample) {
+        return x.map(function(x) {
+          return [x, d3.mean(sample, function(v) { return kernel(x - v); })];
+        });
+      };
+    }
+function epanechnikovKernel(scale) {
+  return function(u) {
+	return Math.abs(u /= scale) <= 1 ? .75 * (1 - u * u) / scale : 0;
+  };
+}
+
+function triweightKernel(scale) {
+  return function(u) {
+	return Math.abs(u /= scale) <= 1 ? (35/32) * Math.pow((1 - u * u), 3) / scale : 0;
+  };
+}
+
+function triangularKernel(scale) {
+  return function(u) {
+	return Math.abs(u /= scale) <= 1 ? Math.abs(1 - u) / scale : 0;
+  };
+}
+
+function uniformKernel(scale) {
+  return function(u) {
+	return Math.abs(u /= scale) <= 1 ? (1/2) / scale : 0;
+  };
+}
+
+function quarticKernel(scale) {
+  return function(u) {
+	return Math.abs(u /= scale) <= 1 ? (15/16) * Math.pow((1 - u * u), 2) / scale : 0;
+  };
+}
+
+function gaussianKernel(scale) {
+  return function(u) {
+	return Math.abs(u /= scale) <= 1 ? (1 / Math.sqrt(2 * Math.PI)) * Math.pow(Math.E, (-1/2) * (u * u)) / scale : 0;
+	//return (1 / Math.sqrt(2 * Math.PI)) * Math.pow(Math.E, (-1/2) * (u * u));
+  };
+}
+//// Wrappers around D3's min/max functions. Return the min or max of an array.
+//
 var minwrap = function(arr) { return d3.min(arr); };
 var maxwrap = function(arr) { return d3.max(arr); };
+
+var DensityPlot = (function() {
+
+	// All publicly exposed functions are added to my.
+	var my = {};
+
+	// 
+	var kernels = {
+
+		EPANECHNIKOV : epanechnikovKernel,
+		TRIWEIGHT : triweightKernel,
+		TRIANGULAR : triangularKernel,
+		UNIFORM : uniformKernel,
+		QUARTIC : quarticKernel
+	};
+
+	function validateOptions(opts, data) {
+
+		// General options
+		var height = opts.height || 400;
+		var width = opts.width || 800;
+		var title = opts.title || 'Generic Density Plot';
+		var xlabel = opts.xlabel || 'X Axis';
+		var ylabel = opts.ylabel || 'Y Axis';
+
+		// Options specific to this visualization
+		// Specifies the kernel function to use when estimating density
+		var kernel = opts.kernel || kernels.TRIWEIGHT;
+		// The smoothing scale to use with the kernel function
+		var scale = opts.scale || 1;
+
+		// The x-domain (min, max)
+		var xmin = opts.xdomain || d3.min(data.map(minwrap));
+		var xmax = opts.xdomain || d3.max(data.map(maxwrap));
+
+		return {
+			height : height,
+			width : width,
+			title : title,
+			xlabel : xlabel,
+			ylabel : ylabel,
+			kernel : kernel,
+			scale : scale,
+			xdomain : [xmin, xmax]
+		};
+	}
+
+	//// linepts : [object]
+	////	object :
+	////		x : int, the x coordinate to draw a line at
+	////		color : line color, default is set to black
+	my.density = function(data, opts, grps, linepts) {
+
+		opts = validateOptions(opts, data);
+
+		var margin = {top: 20, right: 30, bottom: 30, left: 40};
+		var height = opts.height - margin.top - margin.bottom;
+		var width = opts.width - margin.left - margin.right;
+
+		// Normally we'd use the d3js color scale, but I want to pick pretty colors
+		// Might add more later, but I don't think I'll need more than this. 
+		var colors = ['#1f77b4', '#2ca02c', '#d62728', '#9467bd', '#ff7f0e'];
+
+		var x = d3.scale.linear()
+			.domain([opts.xdomain[0], opts.xdomain[1]])
+			.range([0, width]);
+
+		var xAxis = d3.svg.axis()
+			.scale(x)
+			.orient("bottom");
+
+		var kde = kernelDensityEstimator(triweightKernel(opts.scale), x.ticks(50));
+
+		var flat = [].concat.apply([], data);
+		var yvals = kde(flat).map(function(e, i, a) { return e[1]; });
+		var ymin = d3.min(yvals);
+		var ymax = d3.max(yvals);
+
+		var y = d3.scale.linear()
+			.domain([ymin, ymax])
+			.range([height - 1, 0]);
+
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left")
+			;
+
+		var line = d3.svg.line()
+			.x(function(d) { return x(d[0]); })
+			.y(function(d) { return y(d[1]); });
+
+		var area = d3.svg.area()
+			.x(function(d) { return x(d[0]); })
+			.y0(height)
+			.y1(function(d) { return y(d[1]); })
+			;
+
+		var svg = d3.select("body").append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis)
+			.append("text")
+			.attr("class", "label")
+			.attr("x", width)
+			.attr("y", -6)
+			.style("text-anchor", "end")
+			.text(opts.xlabel)
+			;
+
+		svg.append('text')
+			.attr('class', 'label')
+			.style('text-anchor', 'middle')
+			.attr('x', width / 2)
+			.attr('y', 15)
+			.text(opts.title);
+
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis);
+
+		if (grps !== undefined && grps) {
+
+			var key = d3.select('body')
+				.append('svg')
+				.attr('width', 450)
+				.attr('height', 320);
+
+			key.append('text')
+				.attr({
+					'text-anchor': 'middle',
+					'font-family': 'sans-serif',
+					'font-size': '15px',
+					'y': '15',
+					'x': (450 / 3)
+				})
+				.text('Key');
+					
+			var k = key.selectAll('g')
+				.data(grps)
+				.enter()
+				.append('g')
+				.attr('class', 'legend');
+
+			k.append('circle')
+				.attr('cx', 10)
+				.attr('cy', function(d, i){ return (i * 30) + 30; })
+				.attr('r', 10)
+				.attr('fill', function(d, i){
+					return colors[i];
+				})
+				.attr('shape-rendering', 'geometricPrecision')
+				.style('stroke', '#000000')
+				.style('stroke-width', 1);
+
+			k.append('text')
+				.attr('x', 30)
+				.attr('y', function(d, i){ return (i * 30) + 34; })
+				.attr('font-family', 'sans-serif')
+				.attr('font-size', '12px')
+				.text(function(d, i){ return d; });
+		}
+
+		for (var i = 0; i < data.length; i++) {
+
+			svg.append("path")
+				.datum(kde(data[i]))
+				.attr("class", "area")
+				.attr("d", area)
+				.style('fill', function() { return colors[i]; })
+				.style('opacity', 0.5)
+				;
+
+			svg.append("path")
+				.datum(kde(data[i]))
+				.attr("class", "line")
+				.attr("d", line)
+				.attr('shape-rendering', 'geometricPrecision')
+				.style('stroke-width', 1)
+				;
+		}
+
+		linepts = [{x: 0.8}, {x: 0.45, color: '#FF0000'}];
+
+		for (var i = 0; i < linepts.length; i++) {
+
+			var start = [linepts[i].x, 0];
+			var end = [linepts[i].x, ymax];
+			var color = linepts[i].color || '#444444';
+
+			var other_line = d3.svg.line()
+				.x(function(d) { return x(d[0]); })
+				.y(function(d) { return y(d[1]); })
+				;
+
+			svg.append("path")
+				.datum([start, end])
+				.style('stroke', color)
+				.style('stroke-width', 2)
+				.attr('shape-rendering', 'crispEdges')
+				.attr("class", "line")
+				.attr("d", line)
+				;
+		}
+	}
+
+	return my;
+
+}(DensityPlot));
+
 
 //// opts {
 //      xdom : [a, b]
@@ -20,6 +280,7 @@ var maxwrap = function(arr) { return d3.max(arr); };
 //
 var density = function(data, opts, grps) {
 
+	console.log('wut');
     //// Default configuration options
     var height = (opts.dimensions === undefined) ? 400 : opts.dimensions[0];
     var width = (opts.dimensions === undefined) ? 500 : opts.dimensions[1];
@@ -198,42 +459,6 @@ var density = function(data, opts, grps) {
       };
     }
 
-    function epanechnikovKernel(scale) {
-      return function(u) {
-        return Math.abs(u /= scale) <= 1 ? .75 * (1 - u * u) / scale : 0;
-      };
-    }
-
-    function triweightKernel(scale) {
-      return function(u) {
-        return Math.abs(u /= scale) <= 1 ? (35/32) * Math.pow((1 - u * u), 3) / scale : 0;
-      };
-    }
-
-    function triangularKernel(scale) {
-      return function(u) {
-        return Math.abs(u /= scale) <= 1 ? Math.abs(1 - u) / scale : 0;
-      };
-    }
-
-    function uniformKernel(scale) {
-      return function(u) {
-        return Math.abs(u /= scale) <= 1 ? (1/2) / scale : 0;
-      };
-    }
-
-    function quarticKernel(scale) {
-      return function(u) {
-        return Math.abs(u /= scale) <= 1 ? (15/16) * Math.pow((1 - u * u), 2) / scale : 0;
-      };
-    }
-
-    function gaussianKernel(scale) {
-      return function(u) {
-        return Math.abs(u /= scale) <= 1 ? (1 / Math.sqrt(2 * Math.PI)) * Math.pow(Math.E, (-1/2) * (u * u)) / scale : 0;
-        //return (1 / Math.sqrt(2 * Math.PI)) * Math.pow(Math.E, (-1/2) * (u * u));
-      };
-    }
 }
 
 
--- venn-new.js
+++ venn-new.js
@@ -1,66 +1,312 @@
 
-function validateOptions(opts) {
-
-    var height = (opts.height === undefined) ? 500 : opts.height;
-    var width = (opts.width === undefined) ? 500 : opts.width;
-
-	return opts;
-}
-
-//// generateCircles
+//var Venn = (function() {
 //
-//// Creates the circle objects based on set sizes.
-//
-function generateCircles(sets) {
+//	// All publicly exposed functions are added to my.
+//	var my = {};
 
-	var max = d3.max(sets, function(set) { return set.size; });
-	var min = d3.min(sets, function(set) { return set.size; });
-	console.log(max);
+	function randInt(min, max) {
 
-	var normalize = d3.scale.linear()
-		.domain([min, max])
-		.range(5, 20);
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
 
-	return sets.map(function(set) {
+	function makeSets(n) {
+
+		var sets = [];
+		var intersects = [];
+
+		for (var i = 0; i < n; i++) {
+
+			sets.append({
+				label : '' + i,
+				size : randInt(10, 20)
+			});
+		}
+
+		for (var i = 0; i < n; i++) {
+			for (var j = i + 1; j < n; j++) {
+
+				var intersection = randInt(5, 10);
+				var union = randInt(min(sets[i].size, sets[j].size), sets[i].size + sets[j].size);
+
+				intersects.append({
+					set0 : i,
+					set1 : j,
+					intersection : intersection,
+					union : union,
+					jaccard : intersection / union,
+				});
+			}
+		}
+
+		return (sets, intersects);
+	}
+
+	function validateOptions(opts, sets) {
+
+		var opacity = (opts.opacity === undefined) ? 0.55 : opts.opacity;
+		var height = (opts.height === undefined) ? 500 : opts.height;
+		var width = (opts.width === undefined) ? 500 : opts.width;
+		var stroke = (opts.stroke === undefined) ? '#000000' : opts.stroke;
+
+		if (opts.colors === undefined || opts.colors.length < sets.length)
+			var colors = d3.scale.category20();
+		else
+			var colors = opts.colors;
 
 		return {
-			x : 20, 
-			y : 20,
-			r : normalize(set.size)
+			colors : colors,
+			opacity : opacity,
+			height : height,
+			width : width,
+			stroke : stroke
 		};
-	});
-}
-//// sets : [object], data for each set in the diagram
-////	object :
-////		label : string, set label
-////		size : int, set size
-//
-//// intersects : [int, int, int], indexes to set list and # of intersections
-//// intersects : [object], set intersection data
-////	object :
-////		set0 : int, index to a set object in the sets list
-////		set1 : int, index to a set object in the sets list
-////		intersection : int, intersection between set 0 & 1
-////		union : int, union between set 0 & 1
-//
-//// opts :
-////	height : int
-////	width : int
-//
-function makeVenn(sets, intersects, opts) {
+	}
 
-	opts = validateOptions(opts);
+	//// generateCircles
+	//
+	//// Creates the circle objects based on set sizes.
+	//
+	function generateCircles(sets) {
 
-    var margin = {top: 10, right: 30, bottom: 30, left: 50};
-	var height = opts.height - margin.top - margin.bottom;
-	var width = opts.width - margin.left - margin.right;
+		var max = d3.max(sets, function(set) { return set.size; });
+		var min = d3.min(sets, function(set) { return set.size; });
 
-	var svg = d3.select('body')
-		.append('svg')
-		//.attr('class', 'box')
-		.attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom)
-		.append('g')
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-		;
-}
+		var normalize = d3.scale.linear()
+			.domain([min, max])
+			.range([25, 85]);
+
+		return sets.map(function(set) {
+		//return sets.forEach(function(elem, i, set) {
+
+			return {
+				cx : 0, 
+				cy : 0,
+				r : normalize(set.size)
+			};
+		});
+	}
+
+	function euclideanDistance(x0, y0, x1, y1) {
+
+		return Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
+	}
+
+	function csc(a) {
+
+		//return 1 / Math.cos(a);
+		return Math.acos(a);
+	}
+
+	// convenience square function
+	function sqr (n) {
+
+		return n * n;
+	}
+
+	// Finds the area of a circular segment with a radius R and triangular
+	// height d.
+	function circleSegmentArea(r, d) {
+
+		return sqr(r) * csc(d / r) - d * Math.sqrt(sqr(r) - sqr(d));
+	}
+
+	function deg2rad(d) {
+
+		return d * Math.PI / 180;
+	}
+
+	function rad2deg(r) {
+
+		return r * 180 / Math.PI;
+	}
+
+	function bisection(f, a, b, tol=0.00000000001, iter=100) {
+
+		if (f(a) == 0)
+			return a;
+
+		if (f(b) == 0)
+			return b;
+
+		for (var i = 0; i < iter; i++) {
+
+			var c = (a + b) / 2;
+
+			if (f(c) == 0 || ((b - a) / 2) < tol)
+				return c;
+
+			if ((f(c) > 0 && f(a) > 0) || (f(c) < 0 && f(a) < 0))
+				a = c;
+			else
+				b = c;
+		}
+	}
+
+	function distanceForIntersections(r0, r1, intersect) {
+
+		if (Math.min(r0, r1) * Math.min(r0,r1) * Math.PI <= intersect)
+			return Math.abs(r0 - r1);
+
+		return bisection(function(dist) {
+			return circleIntersectArea(r0, r1, dist) - intersect;
+		}, 0, r0 + r1);
+	}
+
+    function circleIntegral(r, x) {
+        var y = Math.sqrt(r * r - x * x);
+        return x * y + r * r * Math.atan2(x, y);
+    };
+
+    /** Returns the area of a circle of radius r - up to width */
+    function circleArea(r, width) {
+        return circleIntegral(r, width - r) - circleIntegral(r, -r);
+    };
+
+    /** Returns the overlap area of two circles of radius r1 and r2 - that
+    have their centers separated by distance d. Simpler faster
+    circle intersection for only two circles */
+    function circleIntersectArea(r1, r2, d) {
+        // no overlap
+        if (d >= r1 + r2) {
+            return 0;
+        }
+
+        // completely overlapped
+        if (d <= Math.abs(r1 - r2)) {
+            return Math.PI * Math.min(r1, r2) * Math.min(r1, r2);
+        }
+
+        var w1 = r1 - (d * d - r2 * r2 + r1 * r1) / (2 * d),
+            w2 = r2 - (d * d - r1 * r1 + r2 * r2) / (2 * d);
+        return circleArea(r1, w1) + circleArea(r2, w2);
+    };
+
+	// Returns the area of the intersection between two circles.
+	function circleIntersectAreaOld(r0, r1, d) {
+
+		//console.log('r0: ' + r0);
+		//console.log('r1: ' + r1);
+		if (d === (r0 + r1))
+			return 0;
+
+		else if (r0 === r1) {
+			//console.log('r: ' + ((1/2) * d));
+			//console.log('d: ' + r0);
+			//console.log('a: ' + circleSegmentArea((1/2) * d, r0));
+			return 2 * circleSegmentArea((1/2) * d, r0);
+		}
+
+
+		console.log((sqr(d) + sqr(r0) - sqr(r1)) / (2 * d * r0));
+		var a = sqr(r0) * csc((sqr(d) + sqr(r0) - sqr(r1)) / (2 * d * r0));
+		var b = sqr(r1) * csc((sqr(d) + sqr(r1) - sqr(r0)) / (2 * d * r1));
+		var c0 = (-d + r0 + r1) * (d + r0 - r1) * (d - r0 + r1) * (d + r0 + r1);
+		var c = (1 / 2) * Math.sqrt(c0);
+
+		//console.log('a: ' + a);
+		//console.log('b: ' + b);
+		//console.log('c: ' + c);
+
+		return a + b - c;
+	}
+
+	function getDistanceMatrix(sets, intersects) {
+
+		var matrix = new Array(sets.length);
+
+		for (var i = 0; i < sets.length; i++) {
+
+			matrix[i] = new Array(sets.length);
+
+			for (var j = 0; j < sets.length; j++)
+				matrix[i][j] = 0;
+		}
+
+		for (var i = 0; i < intersects.length; i++) {
+
+			var left = intersects[i].set0; 
+			var right = intersects[i].set1; 
+			var r0 = sets[left].r;
+			var r1 = sets[right].r;
+			var intersect = intersects[i].intersection;
+			var distance = distanceForIntersections(r0, r1, intersect);
+
+			matrix[left][right] = distance;
+			matrix[right][left] = distance;
+		}
+
+		return matrix;
+	}
+
+	function layout(sets, intersects) {
+
+	}
+
+	function initialLayout(sets, intersects) {
+
+	}
+
+	//// sets : [object], data for each set in the diagram
+	////	object :
+	////		label : string, set label
+	////		size : int, set size
+	//
+	//// intersects : [int, int, int], indexes to set list and # of intersections
+	//// intersects : [object], set intersection data
+	////	object :
+	////		set0 : int, index to a set object in the sets list
+	////		set1 : int, index to a set object in the sets list
+	////		intersection : int, intersection between set 0 & 1
+	////		union : int, union between set 0 & 1
+	//
+	//// opts :
+	////	height : int
+	////	width : int
+	//
+	//my.makeVenn = function(sets, intersects, opts={}) {
+	function makeVenn(sets, intersects, opts={}) {
+
+		opts = validateOptions(opts, sets);
+
+		var margin = {top: 10, right: 30, bottom: 30, left: 50};
+		var height = opts.height - margin.top - margin.bottom;
+		var width = opts.width - margin.left - margin.right;
+
+		var svg = d3.select('body')
+			.append('svg')
+			.attr('width', opts.width)
+			.attr('height', opts.height)
+			.append('g')
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+			;
+
+		var circlist = generateCircles(sets);
+
+		var nodes = svg.append('g').selectAll('circle')
+			.data(circlist)
+			.enter()
+			;
+
+		var circles = nodes.append('circle')
+			.attr('r', function(d) { return d.r; })
+			.attr('cx', function(d, i) { return d.cx + (50 * (i + 1)); })
+			.attr('cy', function(d, i) { return d.cy + (50 * (i + 1)); })
+			.style('fill', function(d, i) { return opts.colors(i); })
+			.style('stroke', function(d) { return opts.stroke; })
+			.style('stroke-width', function() { return 2; })
+			.style('fill-opacity', function() { return opts.opacity; })
+			.style('stroke-opacity', function() { return 0.8; })
+			;
+	}
+
+//	return my;
+//
+//}(Venn));
+
+
+
+
+
+
+
+
