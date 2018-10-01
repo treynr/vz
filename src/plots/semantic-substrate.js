7017701c7f32cfc421a236dcc1cfe2d9488cc62b
TR <git@fin0c.net>
2018-10-01 11:30:13 -0400

    Continue building semantic substrate viz and tests

--- src/plots/semantic-substrate.js
+++ src/plots/semantic-substrate.js
@@ -2,14 +2,16 @@
   * file: semantic-substrate.js
   * desc: d3js implementation of 2D graph plots.
   * auth: TR
+  *
+  * TODO: Consider a reimplementation using d3-force.
   */
 
 'use strict';
 
 import {extent} from 'd3-array';
-improt {axisBottom, axisLeft} from 'd3-axis';
+import {axisBottom, axisLeft} from 'd3-axis';
 import {scaleLinear} from 'd3-scale';
-import {select} from 'd3-selection';
+import {select, selectAll} from 'd3-selection';
 
 export default function() {
 
@@ -30,18 +32,38 @@ export default function() {
 
         /** public **/
 
+        // If true, positions x- and y-axis labels along the ends of each axis rather than
+        // centering them outside the axis
+        altAxisLabels = false,
         // Data object containing objects/data to visualize
         data = null,
+        // Transparency for rendered edges
+        edgeOpacity = 0.6,
+        // Edge color
+        edgeStroke = '#999999',
+        // Edge width
+        edgeStrokeWidth = 2,
         // HTML element or ID the SVG should be appended to
         element = 'body',
+        // Font to use when displaying text
+        font = 'sans-serif',
+        // Font size in pixels
+        fontSize = 13,
         // SVG height
         height = 600,
         margin = {top: 50, right: 50, bottom: 50, left: 50},
+        // Color used to fill in nodes
+        nodeFill = '#d62333',
+        // Radius of rendered nodes in the graph
+        nodeRadius = 7,
+        // Color used when drawing strokes around the nodes
+        nodeStroke = '#ffffff',
+        // Width of the stroke around nodes
+        nodeStrokeWidth = 1,
         // SVG object for the plot
         svg = null,
         // SVG width
         width = 600,
-        // domains
         xDomain = null,
         // Text label for the x-axis
         xLabel = '',
@@ -49,14 +71,21 @@ export default function() {
         xLabelPad = 50,
         // d3 scale type to use for the x-axis
         xScaleFunction = scaleLinear,
-
+        // Format string for x-axis ticks
+        xTickFormat = null,
+        // Suggestion for the number of ticks on the x-axis
+        xTicks = 5,
         yDomain = null,
         // Text label for the y-axis
         yLabel = '',
         // Padding in pixels for the x-axis label
         yLabelPad = 50,
         // d3 scale type to use for the y-axis
-        yScaleFunction = scaleLinear
+        yScaleFunction = scaleLinear,
+        // Format string for y-axis ticks
+        yTickFormat = null,
+        // Suggestion for the number of ticks on the y-axis
+        yTicks = 5
         ;
     
     /** private **/
@@ -73,8 +102,8 @@ export default function() {
       */
     let makeScales = function() {
 
-        xDomain = xDomain ? xDomain : extent(data, d => d.x);
-        yDomain = yDomain ? yDomain : extent(data, d => d.y);
+        xDomain = xDomain ? xDomain : extent(data.nodes, d => d.x);
+        yDomain = yDomain ? yDomain : extent(data.nodes, d => d.y);
 
         xScale = xScaleFunction()
             .domain(xDomain)
@@ -87,9 +116,167 @@ export default function() {
             .range([getHeight(), 0]);
     };
 
+    /**
+      * Creates the d3 axis objects for x- and y-axes.
+      */
     let makeAxes = function() {
 
-        xAxis = axisBottom(xScale)
+        xAxis = axisBottom(xScale).ticks(xTicks, xTickFormat);
+        yAxis = axisLeft(yScale).ticks(yTicks, yTickFormat);
+    };
+
+    /**
+      * Renders the x- and y-axes and attaches them to the SVG.
+      */
+    let renderAxes = function() {
+
+        let xAxisObject = svg.append('g')
+            .attr('class', 'x-axis')
+            .attr('transform', `translate(0, ${getHeight()})`)
+            .attr('font', font)
+            .attr('font-size', `${fontSize}px`)
+            .attr('font-weight', 'normal')
+            .call(xAxis);
+
+        // Attach the x-axis label
+        xAxisObject.append('text')
+            .attr('class', 'x-axis-label')
+            .attr('fill', '#000000')
+            .attr('text-anchor', altAxisLabels ? 'end' : 'middle')
+            //.attr('transform', `translate(0, ${xLabelPad})`)
+            .attr('x', altAxisLabels ? getWidth() : (margin.left + getWidth()) / 2)
+            //.attr('y', altAxisLabels ? -5 : 45)
+            .attr('y', () => {
+                // My weak ass attempt at providing sane defaults for the label position
+                if (xLabelPad !== null)
+                    return xLabelPad;
+
+                return altAxisLabels ? -5 : 45;
+            })
+            .text(xLabel);
+
+        let yAxisObject = svg.append('g')
+            .attr('class', 'y-axis')
+            .attr('transform', `translate(${margin.left}, 0)`)
+            .attr('font', font)
+            .attr('font-size', `${fontSize}px`)
+            .attr('font-weight', 'normal')
+            .call(yAxis);
+
+        // Attach the y-axis label
+        yAxisObject.append('text')
+            .attr('class', 'y-axis-label')
+            .attr('fill', '#000000')
+            .attr('text-anchor', altAxisLabels ? 'start' : 'middle')
+            .attr('transform', altAxisLabels ? '' : 'rotate(-90)')
+            .attr('x', altAxisLabels ? 5 : -getHeight() / 2)
+            .attr('y', () => {
+                if (yLabelPad !== null)
+                    return yLabelPad;
+
+                return altAxisLabels ? 0 : -50;
+            })
+            .text(yLabel);
+    };
+
+    /**
+      * Renders each node in the graph and attaches the objects to the SVG.
+      */
+    let renderNodes = function() {
+
+        let nodes = svg.append('g')
+            .attr('class', 'nodes')
+            .selectAll('nodestuff')
+            .data(data.nodes)
+            .enter()
+            .append('g')
+            .attr('class', 'node');
+
+        nodes.append('circle')
+            .attr('cx', d => xScale(d.x))
+            .attr('cy', d => yScale(d.y))
+            .attr('r', d => {
+
+                if (d.radius)
+                    return d.radius;
+
+                return nodeRadius;
+            })
+            .attr('fill', d => {
+
+                if (d.fill)
+                    return d.fill;
+
+                return nodeFill;
+            })
+            .attr('shape-rendering', 'auto')
+            .attr('stroke', d => {
+
+                if (d.stroke)
+                    return d.stroke;
+
+                return nodeStroke;
+            })
+            .attr('stroke-width', d => {
+
+                if (d.strokeWidth)
+                    return d.strokeWidth;
+
+                return nodeStrokeWidth;
+            });
+    };
+
+    let renderEdges = function() {
+
+        // We have to generate a list of points for each line based on the x, y
+        // coordinates of the node SVG elements. Should think about redoing this using
+        // d3-force.
+        let nodePositions = {};
+
+        //selectAll('.node > circle').each(function(d) {
+        selectAll('.node').each(function(d) {
+
+            let node = select(this);
+
+            //nodePositions[d.id] = {x: node.attr('cx'), y: node.attr('cy')};
+            nodePositions[d.id] = {x: d.x, y: d.y};
+        });
+
+        let edges = svg.append('g')
+            .attr('class', 'edges')
+            .selectAll('edgestuff')
+            .data(data.edges)
+            .enter()
+            .append('g')
+            .attr('class', 'edge');
+
+        edges.append('line')
+            .attr('x1', d => xScale(nodePositions[d.source].x))
+            .attr('y1', d => yScale(nodePositions[d.source].y))
+            .attr('x2', d => xScale(nodePositions[d.target].x))
+            .attr('y2', d => yScale(nodePositions[d.target].y))
+            .attr('fill', 'none')
+            .attr('opacity', d => {
+
+                if (d.opacity)
+                    return d.opacity;
+
+                return edgeOpacity;
+            })
+            .attr('stroke', d => {
+
+                if (d.stroke)
+                    return d.stroke;
+
+                return edgeStroke;
+            })
+            .attr('stroke-width', d => {
+
+                if (d.strokeWidth)
+                    return d.strokeWidth;
+
+                return edgeStrokeWidth;
+            });
     };
 
     exports.draw = function() {
@@ -105,16 +292,51 @@ export default function() {
 
     exports.svg = function() { return svg; };
 
+    exports.altAxisLabels = function(_) { 
+        if (!arguments.length) return altAxisLabels;
+        altAxisLabels = _;
+        return exports;
+    };
+
     exports.data = function(_) { 
-    //exports.data = function(_) {
         if (!arguments.length) return data;
         data = _;
+        return exports;
+    };
+
+    exports.edgeOpacity = function(_) { 
+        if (!arguments.length) return edgeOpacity;
+        edgeOpacity = +_;
+        return exports;
+    };
+
+    exports.edgeStroke = function(_) { 
+        if (!arguments.length) return edgeStroke;
+        edgeStroke = _;
+        return exports;
+    };
+
+    exports.edgeStrokeWidth = function(_) { 
+        if (!arguments.length) return edgeStrokeWidth;
+        edgeStrokeWidth = +_;
         return exports;
     };
 
     exports.element = function(_) { 
         if (!arguments.length) return element;
         element = _;
+        return exports;
+    };
+
+    exports.font = function(_) { 
+        if (!arguments.length) return font;
+        font = _;
+        return exports;
+    };
+
+    exports.fontSize = function(_) { 
+        if (!arguments.length) return fontSize;
+        fontSize = +_;
         return exports;
     };
 
@@ -127,6 +349,30 @@ export default function() {
     exports.width = function(_) { 
         if (!arguments.length) return width;
         width = +_;
+        return exports;
+    };
+
+    exports.nodeFill = function(_) { 
+        if (!arguments.length) return nodeFill;
+        nodeFill = _;
+        return exports;
+    };
+
+    exports.nodeRadius = function(_) { 
+        if (!arguments.length) return nodeRadius;
+        nodeRadius = +_;
+        return exports;
+    };
+
+    exports.nodeStroke = function(_) { 
+        if (!arguments.length) return nodeStroke;
+        nodeStroke = _;
+        return exports;
+    };
+
+    exports.nodeStrokeWidth = function(_) { 
+        if (!arguments.length) return nodeStrokeWidth;
+        nodeStrokeWidth = +_;
         return exports;
     };
 
@@ -154,6 +400,18 @@ export default function() {
         return exports;
     };
 
+    exports.xTickFormat = function(_) { 
+        if (!arguments.length) return xTickFormat;
+        xTickFormat = _;
+        return exports;
+    };
+
+    exports.xTicks = function(_) { 
+        if (!arguments.length) return xTicks;
+        xTicks = +_;
+        return exports;
+    };
+
     exports.yDomain = function(_) { 
         if (!arguments.length) return yDomain;
         yDomain = _;
@@ -178,12 +436,36 @@ export default function() {
         return exports;
     };
 
+    exports.yTickFormat = function(_) { 
+        if (!arguments.length) return yTickFormat;
+        yTickFormat = _;
+        return exports;
+    };
+
+    exports.yTicks = function(_) { 
+        if (!arguments.length) return yTicks;
+        yTicks = +_;
+        return exports;
+    };
+
     // Enables testing for private methods
     if (process.env.NODE_ENV == 'development') {
 
         exports.makeScales = makeScales;
+        exports.makeAxes = makeAxes;
+        exports.renderAxes = renderAxes;
+        exports.renderNodes = renderNodes;
+        exports.renderEdges = renderEdges;
+        exports.xAxis = () => xAxis;
         exports.xScale = () => xScale;
         exports.yScale = () => yScale;
+        exports.yAxis = () => yAxis;
+
+        exports.svg = function(_) { 
+            if (!arguments.length) return svg;
+            svg = _;
+            return exports;
+        };
     }
 
     return exports;
--- test/semantic-substrate-test.js
+++ test/semantic-substrate-test.js
@@ -6,26 +6,46 @@
 
 const substrate = require('../dist/semantic-substrate');
 const tape = require('tape');
+const d3 = require('d3');
+const jsdom = require('jsdom');
+
+const data = {
+    nodes: [
+        {x: 1, y: 1, value: 0, id: 'a'},
+        {x: 2, y: 3, value: 0, id: 'b'},
+        {x: 4, y: 2, value: 0, id: 'c', stroke: '#000000', strokeWidth: 2},
+        {x: 4, y: 5, value: 0, id: 'd', fill: '#000000'},
+        {x: 5, y: 5, value: 0, id: 'e', radius: 10}
+    ],
+    edges: [
+        {source: 'a', target: 'b'},
+        {source: 'a', target: 'c'},
+        {source: 'b', target: 'c'},
+        {source: 'b', target: 'd'},
+        {source: 'c', target: 'd'},
+        {source: 'd', target: 'e', opacity: 0.8, stroke: '#000000', strokeWidth: 4}
+    ]
+}
 
 tape('makeScales() sets the default x-scale', t => {
 
-    let data = [{x:2}, {x:3}, {x:4}];
+    //let data = [{x:2}, {x:3}, {x:4}];
     let s = substrate().data(data);
 
     s.makeScales();
-    t.equal(s.xScale().domain()[0], 2);
-    t.equal(s.xScale().domain()[1], 4);
+    t.equal(s.xScale().domain()[0], 1);
+    t.equal(s.xScale().domain()[1], 5);
     t.end();
 });
 
 tape('makeScales() sets the default y-scale', t => {
 
-    let data = [{y:30}, {y:20}, {y:10}];
+    //let data = [{y:30}, {y:20}, {y:10}];
     let s = substrate().data(data);
 
     s.makeScales();
-    t.equal(s.yScale().domain()[0], 10);
-    t.equal(s.yScale().domain()[1], 30);
+    t.equal(s.yScale().domain()[0], 1);
+    t.equal(s.yScale().domain()[1], 5);
     t.end();
 });
 
@@ -41,3 +61,181 @@ tape('makeScales() returns correct values for user defined domains', t => {
     t.end();
 });
 
+tape('makeAxes() returns correct ticks for default data sets', t => {
+
+    //let s = substrate().data([{x:1, y:1}, {x:5, y:5}]);
+    let s = substrate().data(data);
+
+    s.makeScales();
+    s.makeAxes();
+    t.deepEqual(s.xAxis().scale().ticks(s.xTicks()), [1, 2, 3, 4, 5]);
+    t.deepEqual(s.yAxis().scale().ticks(s.yTicks()), [1, 2, 3, 4, 5]);
+    t.end();
+});
+
+tape('makeAxes() returns correct ticks for user defined domains', t => {
+
+    let s = substrate().xDomain([1, 10]).yDomain([1, 10]);
+
+    s.makeScales();
+    s.makeAxes();
+    t.deepEqual(s.xAxis().scale().ticks(s.xTicks()), [2, 4, 6, 8, 10]);
+    t.deepEqual(s.yAxis().scale().ticks(s.yTicks()), [2, 4, 6, 8, 10]);
+    t.end();
+});
+
+tape('renderAxes() renders default axes', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).xDomain([1, 10]).yDomain([1, 10]);
+
+    s.makeScales();
+    s.makeAxes();
+    s.renderAxes();
+    t.notOk(d3.select('.x-axis').empty());
+    t.notOk(d3.select('.y-axis').empty());
+    t.ok(d3.select('.x-axis-label').text() == '');
+    t.ok(d3.select('.y-axis-label').text() == '');
+    t.end();
+});
+
+tape('renderAxes() renders custom labels', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).xDomain([1, 10]).yDomain([1, 10]).xLabel('lul').yLabel('lul');
+
+    s.makeScales();
+    s.makeAxes();
+    s.renderAxes();
+    t.ok(d3.select('.x-axis-label').text() == 'lul');
+    t.ok(d3.select('.y-axis-label').text() == 'lul');
+    t.end();
+});
+
+tape('renderNodes() renders default nodes', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).data(data);
+
+    s.makeScales();
+    s.renderNodes();
+    t.notOk(d3.select('.nodes').empty());
+    t.notOk(d3.select('.node').empty());
+    t.ok(d3.selectAll('.node').size() == 5);
+    t.end();
+});
+
+tape('renderNodes() renders nodes with default formatting', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).data(data);
+
+    s.makeScales();
+    s.renderNodes();
+    t.plan(8);
+    d3.selectAll('.node > circle').each(function(d) {
+
+        if (d.id == 'a' || d.id == 'b') {
+
+            t.ok(d3.select(this).attr('fill') === s.nodeFill());
+            t.ok(d3.select(this).attr('r') == s.nodeRadius());
+            t.ok(d3.select(this).attr('stroke') === s.nodeStroke());
+            t.ok(d3.select(this).attr('stroke-width') == s.nodeStrokeWidth());
+        }
+    });
+    t.end();
+});
+
+tape('renderNodes() renders nodes with custom formatting', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).data(data);
+        //{x: 1, y: 1, value: 0, id: 'a'},
+        //{x: 2, y: 3, value: 0, id: 'b'},
+        //{x: 4, y: 2, value: 0, id: 'c', stroke: '#00000', strokeWidth: 2},
+        //{x: 4, y: 5, value: 0, id: 'd', fill: '#000000'},
+        //{x: 5, y: 5, value: 0, id: 'e', radius: 10}
+
+    s.makeScales();
+    s.renderNodes();
+    t.plan(4);
+    d3.selectAll('.node > circle').each(function(d) {
+
+        if (d.id === 'c') {
+
+            t.ok(d3.select(this).attr('stroke') === '#000000');
+            t.ok(d3.select(this).attr('stroke-width') == 2);
+        }
+
+        if (d.id === 'd')
+            t.ok(d3.select(this).attr('fill') === '#000000');
+
+        if (d.id === 'e')
+            t.ok(d3.select(this).attr('r') == 10);
+    });
+    t.end();
+});
+
+tape('renderEdges() renders default edges', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).data(data);
+
+    s.makeScales();
+    s.renderNodes();
+    s.renderEdges();
+    t.notOk(d3.select('.edges').empty());
+    t.notOk(d3.select('.edge').empty());
+    t.ok(d3.selectAll('.edge').size() == 6);
+    t.end();
+});
+
+tape('renderEdges() renders edges with default formatting', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).data(data);
+
+    s.makeScales();
+    s.renderNodes();
+    s.renderEdges();
+    t.plan(15);
+    d3.selectAll('.edge > line').each(function(d) {
+
+        if (d.opacity === undefined) {
+
+            t.ok(d3.select(this).attr('opacity') == s.edgeOpacity())
+            t.ok(d3.select(this).attr('stroke') === s.edgeStroke());
+            t.ok(d3.select(this).attr('stroke-width') == s.edgeStrokeWidth());
+        }
+    });
+    t.end();
+});
+
+tape('renderNodes() renders nodes with custom formatting', t => {
+
+    global.document = (new jsdom.JSDOM(`...`)).window.document;
+    let svg = d3.select('body').append('svg');
+    let s = substrate().svg(svg).data(data);
+
+    s.makeScales();
+    s.renderNodes();
+    s.renderEdges();
+    t.plan(3);
+    d3.selectAll('.edge > line').each(function(d) {
+
+        if (d.opacity !== undefined) {
+
+            t.ok(d3.select(this).attr('opacity') == 0.8)
+            t.ok(d3.select(this).attr('stroke') === '#000000')
+            t.ok(d3.select(this).attr('stroke-width') == 4)
+        }
+    });
+    t.end();
+});
